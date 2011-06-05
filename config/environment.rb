b7df90cdff43fd9f33d5baf43f201b1a7877c780
captproton <carl@captproton.com>
2011-06-04 19:14:12 -0700

    moved host mailing config to production enviro because it more proper

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DeviseExample::Application.initialize!
-config.action_mailer.default_url_options = { :host => 'mycoupons.heroku.com' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,8 @@ DeviseExample::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'mycoupons.heroku.com' }
+  
 
   # Enable threaded mode
   # config.threadsafe!
